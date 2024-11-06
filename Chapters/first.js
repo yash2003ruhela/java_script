ad702e5165ba0da5b09c02459b96edbeead4a4fa
yash2003ruhela <124277410+yash2003ruhela@users.noreply.github.com>
2024-11-06 20:44:52 +0530

    js 1 commit

---
+++ Chapters/first.js
@@ -0,0 +1,38 @@
+// console.log("First");
+//variables?
+// variables are the containers for data
+// js is a dynamically types language hai
+// Rules 
+// 1=>it is a case sensitive 
+// 2=>only letters,digitd,underscore,dollar sign is allowed(even not spce)
+// 3=>only letter,underscore and dollar will be the first letter
+// 4=>reserved words can not be a variable
+// 5=>always right in cammel case (it is a convension)
+
+
+// let , comst , var
+
+// var=>variable can be re-declared and updated. a global sco[e variable]
+// let=>variable can not be re declared but can be updated .a block scope variable
+// const=>variable can not be re declared and updated . ablock scope variable
+
+// let name="tony stark";
+// let agr=24;
+// let totalPrice=19000;
+// console.log(name);
+// let a;
+// console.log(a);
+
+
+// //dataTypes in js
+// 1=>Number , String , Boolean , Null, undefined ,BigInt, Symbol
+// Premitive data types=7
+// Non-premitive data types=Objects =>it is a collection of values key:value;
+
+
+// const student={
+//     fullName:"yashRuhela",
+//     agr:21,
+//     cpi:8.9,
+//     isPass:true, 
+
